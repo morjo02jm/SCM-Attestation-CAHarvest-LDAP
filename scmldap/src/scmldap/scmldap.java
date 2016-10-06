891ecef06c762f26bd51445e8649c416ad2d2c79
Donald Faul <faudo01@ca.com>
2016-10-06 10:59:45 -0700

    Decommissioning of cscr604

--- scmldap/src/scmldap/scmldap.java
+++ scmldap/src/scmldap/scmldap.java
@@ -395,7 +395,7 @@ public class scmldap implements IJCaLogStreamListener
 			"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr601;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
 			"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr602;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
 			"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr603;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
-			"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr604;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
+			//"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr604;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
 			"jdbc:sqlserver://L1AGUSDB004P-1;databaseName=cscr605;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
 			"jdbc:sqlserver://L1AGUSDB004P-1;databaseName=cscr606_12.5;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
 			//"jdbc:sqlserver://L1AGUSDB003P-1;databaseName=cscr607;integratedSecurity=false;selectMethod=cursor;multiSubnetFailover=true;user=harvest;password=V724TF@y3Adawt$;",
